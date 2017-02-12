48ea42d4f62c3d869b721509b47d7cd929a8fef9
Jerryhal <jerry.hallfors@gmail.com>
2017-02-13 00:02:39 +0200

    Add files via upload

--- Assets/GameControl.cs
+++ Assets/GameControl.cs
@@ -5,26 +5,44 @@ using UnityEngine.UI;
 
 public class GameControl : MonoBehaviour {
 
-	GameObject playerObject; 
+	GameObject patientPlayerObject; 
+	GameObject thiefPlayerObject; 
+	GameObject currentPlayerObject;
 	ButtonController rightButton;
 	ButtonController leftButton;
-	Vector3 playerLocation;
+	Vector3 patientPlayerLocation;
 	Text points;
-	Player player;
+	Player patientPlayer;
+	Player currentPlayer;
+	Player thiefPlayer;
 	int jumpHeight; //Kaikki tälläset pelaajalle omaiset "statsit" sirretään mahdollisesti Player luokkaan myöhemmin
 	bool jumpCap;
 	int jumpProgress;
-//	Camera camera; 
+	Camera camera;
+	Vector3 offset;
+	Canvas gameOver;
+	Canvas pauseCanvas;
+	Text GameOverText;
+
 
 	// Use this for initialization
 	void Start () {
+		gameOver = GameObject.Find ("GameOverCanvas").GetComponent<Canvas> ();
+		pauseCanvas = GameObject.Find ("PauseCanvas").GetComponent<Canvas> ();
 		points = GameObject.Find ("PointsText").GetComponent<Text> ();
-		playerObject = GameObject.Find ("Player"); 
-		playerLocation = playerObject.transform.position; 
-		player = GameObject.Find("Player").GetComponent<Player>();
+		patientPlayerObject = GameObject.Find ("PatientPlayer"); 
+		thiefPlayerObject = GameObject.Find ("ThiefPlayer"); 
+		patientPlayerLocation = patientPlayerObject.transform.position;
+		patientPlayer = GameObject.Find("PatientPlayer").GetComponent<Player>();
+		thiefPlayer = GameObject.Find("ThiefPlayer").GetComponent<Player>();
 		jumpCap = false;
-		jumpHeight = 5; //Hypyn max korkeus
+		pauseCanvas.enabled = false;
+		jumpHeight = 30; //Hypyn max korkeus
 		int jumpProgress = 0;
+		camera = GameObject.Find ("Main Camera").GetComponent<Camera> ();
+		currentPlayer = patientPlayer;
+		currentPlayerObject = patientPlayerObject;
+		offset = camera.transform.position - currentPlayerObject.transform.position;
 //		rightButton = GameObject.Find ("RightButton").GetComponent<ButtonController>(); 
 //		leftButton = GameObject.Find ("LeftButton").GetComponent<ButtonController>(); Mahdollisia virtuaali nappeja varten
 
@@ -32,49 +50,119 @@ public class GameControl : MonoBehaviour {
 	}
 		
 	// Update is called once per frame
+
 	void Update (){
 		
+		camera.transform.position = currentPlayerObject.transform.position + offset; //Kamera seuraa pelaajaa
+
 		//Input.GetKey (KeyCode.nappi) niin kauan kun nappia painetaan niin suoritetaan komentoa
 		//Input.GetKeyDown (KeyCode.nappi) kun nappia painetaan kerran niin suoritetaan komento
-		//To do: viisto hyppy, disablee airstrafe
 
-		if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.A) && jumpCap == false) {
-			playerObject.transform.Translate (-50, 0, 0);
-			Jump ();
+		if (gameOver.enabled == false && pauseCanvas.enabled == false) { //Jos pelaaja ei ole game over tai pause screenissä niin voi liikkua
 
-		} else if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D) && jumpCap == false) {
-			playerObject.transform.Translate (50, 0, 0);
-			Jump ();
+			if (Input.GetKeyUp (KeyCode.W)) { //Jos W nostetaan pohjasta, jumpCap = true -> Pelaaja ei voi leijua spämmäämällä W:tä
+				jumpCap = true;
 
-		} else if (Input.GetKey (KeyCode.W) && jumpCap == false) { //kun W painetaan pohjassa ja jumpCap == false pelaaja hyppää 
-			Jump ();
+			} else if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.A) && jumpCap == false) { //Viisto hyppy vasemmalle
+				if (currentPlayer.Facing () == false) {
+					currentPlayerObject.transform.Translate (30, 0, 0);
+				} else {
+					Flip ();
+				}
+				Jump ();
+
+			} else if (Input.GetKey (KeyCode.W) && Input.GetKey (KeyCode.D) && jumpCap == false) { //Viisto hyppy oikealle
+				if (currentPlayer.Facing () == true) {
+					currentPlayerObject.transform.Translate (30, 0, 0);
+				} else {
+					Flip ();
+				}
+				Jump ();
+
+			} else if (Input.GetKey (KeyCode.W) && jumpCap == false) { //kun W painetaan pohjassa ja jumpCap == false pelaaja hyppää 
+				Jump ();
+
+			} else if (Input.GetKey (KeyCode.D)) { //liiku oikealle
+				if (currentPlayer.Facing () == true) {
+					currentPlayerObject.transform.Translate (10, 0, 0);
+				} else {
+					Flip ();
+				}
+
+			} else if (Input.GetKey (KeyCode.A)) { //Liiku vasemmalle
+				if (currentPlayer.Facing () == false) {
+					currentPlayerObject.transform.Translate (10, 0, 0);
+				} else {
+					Flip ();
+				}
+
+			} else if (Input.GetKeyDown (KeyCode.Tab)) {//Tab vaihtaa hahmoa 
+				if (currentPlayer == patientPlayer) {
+					currentPlayer = thiefPlayer;
+					currentPlayerObject = thiefPlayerObject;
+				} else {
+					currentPlayer = patientPlayer;
+					currentPlayerObject = patientPlayerObject;
+				}
+
+			}
+		}
+		if (Input.GetKeyDown (KeyCode.Escape)  && gameOver.enabled == false) { //Esc Pausee pelin 
+				Pause ();
 		}
 
-		if (jumpHeight == 5) { //jumpHeight == 5 toistaiseksi, koska se on true enkä jaksa editoida. if:in sisään player.GetGrounded () == true jos halutaan disablee airstrafe
+		if (currentPlayer.GetHealth () <= 0) {
+			GameOver ();
+				}
+		points.text = "Points: " + (thiefPlayer.GetPoints() + patientPlayer.GetPoints()); //Pitää näytöllä lukua kerätyistä kolikoista
+			}
 
-			if (Input.GetKey (KeyCode.D)) {
-			playerObject.transform.Translate (5, 0, 0);
+	void Jump() { //Hyppy
+		//Liikutetaan pelaajaa ylöspäin kunnes saavutetaan hypyn lakipiste, jolloin pelaaja lakkaa liikkumasta ylös.
+		currentPlayerObject.transform.Translate (0, 20, 0); //Hypyn "räjähtävyys"
 
-			} else if (Input.GetKey (KeyCode.A)) {
-			playerObject.transform.Translate (-5, 0, 0);
+		currentPlayer.SetGrounded (false); //Pelaaja ei ole enää maassa 
+			jumpProgress++;
+		if (jumpProgress == jumpHeight) { //Hypyn lakipiste saavutettu
+				jumpCap = true; //Pelaaja ei voi hypätä uudelleen kunnes jumpCap == false
+				jumpProgress = 0; 
 			}
 		}
 
-		points.text = "Points: " + player.GetPoints (); //Pitää näytöllä lukua kerätyistä kolikoista 
-	} 
-
-	void Jump() {
-		//Liikutetaan pelaajaa ylöspäin kunnes saavutetaan hypyn lakipiste, jolloin pelaaja lakkaa liikkumasta ylös. 
-		playerObject.transform.Translate (0, 50, 0); //Hypyn "räjähtävyys"
-		player.SetGrounded(false); // 
-		jumpProgress++;
-		if (jumpProgress == jumpHeight) { //Hypyn lakipiste saavutettu
-			jumpCap = true; //Pelaaja ei voi hypätä uudelleen kunnes jumpCap == false
-			jumpProgress = 0;
-		}
-	}
 
 	public void SetJumpCap(bool b) { 
 		this.jumpCap = b;
+	}
+
+	public GameObject GetPlayer() {
+		return this.currentPlayerObject;
+	}
+
+	void GameOver() {
+		Time.timeScale = 0; //Pausee pelin 
+		gameOver.enabled = true; //Game over kanvas tulee esiin
+
+	}
+
+	void Pause() {
+		if (pauseCanvas.enabled == true) {
+			Time.timeScale = 1; //Palauttaa peliajan normaaliksi
+			pauseCanvas.enabled = false; //Pause kanvas tulee esiin
+
+		} else if (pauseCanvas.enabled == false) {
+			Time.timeScale = 0; //Pysäyttää peliajan
+			pauseCanvas.enabled = true; //Pause kanvas tulee esiin
+		}	
+	}
+
+	void Flip() {
+
+		if (currentPlayerObject == patientPlayerObject) { 
+			patientPlayerObject.transform.Translate (-200, 0, 0); //Tasaa flipin yhteydessä tapahtuvaa "nykäisyiä". Modeli kohtainen arvo.
+		} else if (currentPlayerObject == thiefPlayerObject) {
+			thiefPlayerObject.transform.Translate (-1000, 0, 0);  //Tasaa flipin yhteydessä tapahtuvaa "nykäisyiä". Modeli kohtainen arvo.
+		}
+		currentPlayerObject.transform.Rotate (0, 180, 0); //Modeli peilaantuu
+		currentPlayer.SetFacing (!currentPlayer.Facing()); //player.Facing saa vastakkaisen arvon. 
 	}
 }
--- Assets/Player.cs
+++ Assets/Player.cs
@@ -8,15 +8,44 @@ public class Player : MonoBehaviour {
 	GameControl controls;
 	GameObject coin; //Siirretään mahdollisesti GameItems (tai vast.) luokkaan
 	bool grounded;
+	bool freeze;
+	bool facing;
+	int health; 
+
+
+	// Use this for initialization
+	void Start () {
+		freeze = false;
+		controls = GameObject.Find("GameControl").GetComponent<GameControl>(); //Haetaan referenssi GameControl scriptiin 
+		grounded = true;  
+		facing = true;
+		health = 100; 
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+
+	}
 
 	void OnCollisionEnter2D(Collision2D collision) { //Kun pelaaja osuu collideriin tapahtuu seuraava: 
 		//collision muuttuja on collideri mihin pelaaja koskee 
-		Debug.Log (collision.gameObject.name);
+		Debug.Log(collision); 
 
 		if (collision.gameObject.name == "Coin") { //collision.gameObject.name == "spriteen liitetyn _Kuvan_ nimi"
 			collision.collider.gameObject.SetActive (false); //Kolikko katoaa pelimaailmasta
 			GameObject.Destroy (collision.collider.gameObject); //Kolikko katoaa unityn muistista
 			points++;
+
+		} else if (collision.gameObject.name == "Spikes") {
+			health -= 35; //piikkien damage
+			if (health > 0) { //pelaaja ponnahtaa iskusta vain jos isku ei ole tappava
+				gameObject.transform.Translate (0, 300, 0); //pelaaja ponnahtaa ylös 
+			}
+			Debug.Log("health: "+ health);
+			controls.SetJumpCap (false); //piikkien koskeminen lasektaan maassa käymiseksi,
+			grounded = true;			// joten pelaaja voi hyppää uudelleen välttääkseen putoamasta piikkien päälle uudestaan. 
 
 		} else { //Suosittelen antamaan kaikille tasanne tai maa collideri kuville samat nimet 
 			controls.SetJumpCap (false); //Kun pelaaja osuu Collideriin, mikä ei ole kolikko, jumpCap = false -> pelaaja voi hypätä uudelleen
@@ -37,16 +66,18 @@ public class Player : MonoBehaviour {
 		this.grounded = b;
 	}
 
-	// Use this for initialization
-	void Start () {
-		controls = GameObject.Find("GameControl").GetComponent<GameControl>(); //Haetaan referenssi GameControl scriptiin 
-		this.grounded = true;  
-
+	public int GetHealth() {
+		return health;
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-		
+
+	public bool Facing() {
+		return facing;
+	}
+
+	public void SetFacing(bool b) {
+		facing = b;
+	}
+
+	public void SetAlive(bool b) {
 	}
 }
